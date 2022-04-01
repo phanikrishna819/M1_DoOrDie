8382c94cab8d85b0db43ed523701f4e20b50e7a2
K. S. Phani Krishna <57516776+phanikrishna819@users.noreply.github.com>
2022-04-01 11:16:23 +0530

    Add files via upload

---
+++ MiniProject_C/3_Implementation/test_addint.c
@@ -0,0 +1,21 @@
+#include "unity.h"
+#include<stdio.h>
+#include"game.h"
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+
+int main(void)
+{
+    printf("\t****DO\tOR  DIE****\n\n*RULES: SELECT A CASE AND ENTER TWO NUMBERS IF U WON COLLECT THE PRIZE!!\n\n \t**YOU GOT ONLY ONE CHANCE**\n\n");
+    UnityBegin(NULL);
+    RUN_TEST(function);
+    /*TEST_ASSERT_EQUAL(0,add(0,0));
+    TEST_ASSERT_EQUAL(10,add(15,10));*/
+    return(UnityEnd());
+}
