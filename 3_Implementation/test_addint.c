98b63d0a9a04b718e653cef66925398ae51e1d14
K. S. Phani Krishna <57516776+phanikrishna819@users.noreply.github.com>
2022-04-02 16:57:24 +0530

    Update test_addint.c

--- 3_Implementation/test_addint.c
+++ 3_Implementation/test_addint.c
@@ -13,7 +13,7 @@ void tearDown(void)
 
 int main(void)
 {
-    printf("\t****DO\tOR  DIE****\n\n*RULES: SELECT A CASE AND ENTER TWO NUMBERS IF U WON COLLECT THE PRIZE!!\n\n \t**YOU GOT ONLY ONE CHANCE**\n\n");
+    printf("\t****GuessMe****\n\n*RULES: SELECT A CASE AND ENTER TWO NUMBERS IF U WON COLLECT THE PRIZE!!\n\n \t**YOU GOT ONLY ONE CHANCE**\n\n");
     UnityBegin(NULL);
     RUN_TEST(function);
     /*TEST_ASSERT_EQUAL(0,add(0,0));
